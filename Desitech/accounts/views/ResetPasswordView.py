a8c0b448497d19661de94d1a3cc8ac233b63d7f1
ali <Ali1995Askar>
2020-08-25 02:06:04 +0300

    host 9

--- Desitech/accounts/views/ResetPasswordView.py
+++ Desitech/accounts/views/ResetPasswordView.py
@@ -39,7 +39,7 @@ class ResetPassword (View):
 					"user": user,
 					'full_user_name' : name, 
 					"email":user.email,
-					'domain':'127.0.0.1:8000',
+					'domain':'http://aliaskar1333.pythonanywhere.com/',
 					'site_name': 'Desitech',
 					'uid': urlsafe_base64_encode(force_bytes(user.pk)),
 					'token': default_token_generator.make_token(user),
